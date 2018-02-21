5abab23b420cacaee511af9df1da086103bc971f
RAK <rak@RAKui-MacBook-Pro.local>
2018-02-21 14:50:52 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
+
+  get 'home/new'
+
+  post 'home/create'
+
+  get 'home/destroy/:post_id', to: 'home#destroy', as: 'post_destroy'
+
+  get 'home/edit/:post_id' => 'home#edit'
+
+  post 'home/update/:post_id' => 'home#update'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
